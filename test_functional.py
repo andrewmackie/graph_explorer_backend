f2b4c309617df9fec8f8b1ef437687dde471d888
unknown <1217203+andrewmackie@users.noreply.github.com>
2020-10-12 11:18:57 +1100

    Updated tests

--- test_functional.py
+++ test_functional.py
@@ -49,118 +49,118 @@ def return_item_with_id(list_of_dictionaries, id):
 
 def test_graph_get(client):
     response = client.get('/api/v0/graph')
-    assert response.status_code == 200 and \
-           len(response.json['nodes']) == 10 and \
-           len(response.json['edges']) == 9
+    assert response.status_code == 200
+    assert len(response.json['nodes']) == 10
+    assert len(response.json['edges']) == 9
 
 
 def test_node_get(client):
     response = client.get('/api/v0/node/1')
-    assert response.status_code == 200 and \
-           response.json['id'] == 1 and \
-           response.json['name'] == 'Node1' and \
-           response.json['_color']
+    assert response.status_code == 200
+    assert response.json['id'] == 1
+    assert response.json['name'] == 'Node1'
+    assert response.json['_color']
 
 
 def test_node_post(client):
     response = client.post('/api/v0/node', json={'name': name, '_color': color})
-    assert response.status_code == 201 and \
-           len(response.json['nodes']) == 11 and \
-           response.json['nodes'][10]['name'] == name and \
-           response.json['nodes'][10]['_color'] == color
+    assert response.status_code == 201
+    assert len(response.json['nodes']) == 11
+    assert response.json['nodes'][10]['name'] == name
+    assert response.json['nodes'][10]['_color'] == color
 
 
 def test_node_put(client):
     response = client.put('/api/v0/node/1', json={'name': name, '_color': color})
     updated_node = return_item_with_id(response.json['nodes'], 1)
-    assert response.status_code == 200 and \
-           updated_node['name'] == name and \
-           updated_node['_color'] == color
+    assert response.status_code == 200
+    assert updated_node['name'] == name
+    assert updated_node['_color'] == color
 
 
 def test_node_put_partial_1(client):
     existing_node_data = client.get('/api/v0/node/1').json
     response = client.put('/api/v0/node/1', json={'name': name})
     updated_node = return_item_with_id(response.json['nodes'], 1)
-    assert response.status_code == 200 and \
-           updated_node['name'] == name and \
-           updated_node['_color'] == existing_node_data['_color']
+    assert response.status_code == 200
+    assert updated_node['name'] == name
+    assert updated_node['_color'] == existing_node_data['_color']
 
 
 def test_node_put_partial_2(client):
     existing_node_data = client.get('/api/v0/node/1').json
     response = client.put('/api/v0/node/1', json={'_color': color})
     updated_node = return_item_with_id(response.json['nodes'], 1)
-    assert response.status_code == 200 and \
-           updated_node['name'] == existing_node_data['name'] and \
-           updated_node['_color'] == color
+    assert response.status_code == 200
+    assert updated_node['name'] == existing_node_data['name']
+    assert updated_node['_color'] == color
 
 
 def test_node_delete(client):
     response = client.delete('/api/v0/node/1')
-    assert response.status_code == 200 and \
-           len(response.json['nodes']) == 9 and \
-           return_item_with_id(response.json['edges'], 1) == None
+    assert response.status_code == 200
+    assert len(response.json['nodes']) == 9
+    assert return_item_with_id(response.json['edges'], 1) == None
 
 
 def test_edge_get(client):
     response = client.get('/api/v0/edge/1')
-    assert response.status_code == 200 and \
-           response.json['id'] == 1 and \
-           response.json['name'] == 'Edge-1-2' and \
-           response.json['sid'] == 1 and \
-           response.json['tid'] == 2 and \
-           response.json['_color']
+    assert response.status_code == 200
+    assert response.json['id'] == 1
+    assert response.json['name'] == 'Edge-1-2'
+    assert response.json['sid'] == 1
+    assert response.json['tid'] == 2
+    assert response.json['_color']
 
 
 def test_edge_post(client):
     response = client.post('/api/v0/edge', json={'sid': sid, 'tid': tid, 'name': name, '_color': color})
     new_edge = return_item_with_id(response.json['edges'], 10)
-    assert response.status_code == 201 and \
-           len(response.json['edges']) == 10 and \
-           new_edge['sid'] == sid and \
-           new_edge['tid'] == tid and \
-           new_edge['name'] == name and \
-           new_edge['_color'] == color
+    assert response.status_code == 201
+    assert len(response.json['edges']) == 10
+    assert new_edge['sid'] == sid
+    assert new_edge['tid'] == tid
+    assert new_edge['name'] == name
+    assert new_edge['_color'] == color
 
 
 def test_edge_put(client):
     response = client.put('/api/v0/edge/1', json={'sid': sid, 'tid': tid, 'name': name, '_color': color})
     updated_edge = return_item_with_id(response.json['edges'], 1)
-    assert response.status_code == 200 and \
-           updated_edge['sid'] == sid and \
-           updated_edge['tid'] == tid and \
-           updated_edge['name'] == name and \
-           updated_edge['_color'] == color
+    assert response.status_code == 200
+    assert updated_edge['sid'] == sid
+    assert updated_edge['tid'] == tid
+    assert updated_edge['name'] == name
+    assert updated_edge['_color'] == color
 
 
 def test_edge_put_partial_1(client):
     existing_edge_data = client.get('/api/v0/edge/1').json
     response = client.put('/api/v0/edge/1', json={'sid': sid, 'name': name})
     updated_edge = return_item_with_id(response.json['edges'], 1)
-    assert response.status_code == 200 and \
-           updated_edge['sid'] == sid and \
-           updated_edge['tid'] == existing_edge_data['tid'] and \
-           updated_edge['name'] == name and \
-           updated_edge['_color'] == existing_edge_data['_color']
+    assert response.status_code == 200
+    assert updated_edge['sid'] == sid
+    assert updated_edge['tid'] == existing_edge_data['tid']
+    assert updated_edge['name'] == name
+    assert updated_edge['_color'] == existing_edge_data['_color']
 
 
 def test_edge_put_partial_2(client):
     existing_node_data = client.get('/api/v0/edge/1').json
     response = client.put('/api/v0/edge/1', json={'tid': tid, '_color': color})
     updated_edge = return_item_with_id(response.json['edges'], 1)
-    assert response.status_code == 200 and \
-           updated_edge['sid'] == existing_node_data['sid'] and \
-           updated_edge['tid'] == tid and \
-           updated_edge['name'] == existing_node_data['name'] and \
-           updated_edge['_color'] == color
+    assert response.status_code == 200
+    assert updated_edge['sid'] == existing_node_data['sid']
+    assert updated_edge['tid'] == tid
+    assert updated_edge['name'] == existing_node_data['name']
+    assert updated_edge['_color'] == color
 
 
 def test_edge_delete(client):
     response = client.delete('/api/v0/edge/1')
-    assert response.status_code == 200 and \
-           len(response.json['edges']) == 8 and \
-           return_item_with_id(response.json['edges'], 1) == None
+    assert response.status_code == 200
+    assert len(response.json['edges']) == 8
+    assert return_item_with_id(response.json['edges'], 1) == None
 
 
 def test_swag(client):
@@ -173,7 +173,6 @@ def test_swag(client):
     specs_data = client.get('/apispec_1.json').json
     print(specs_data)
     #for url, spec in specs_data.items():
-    assert 'Node' in specs_data['components']['schemas'] and \
-    'Edge' in specs_data['components']['schemas']
-        # 'route '/colors/<palette>/' becomes '/colors/{palette}/'
-        #assert 'colors' in spec['paths']['/colors/{palette}/']['get']['tags']
+    assert 'node' in specs_data['components']['schemas']
+    assert 'edge' in specs_data['components']['schemas']
+    assert 'graph' in specs_data['components']['schemas']
